6929fce0e7b82550093d2dff26105ddbba5d6a2b
tb <tb@toolblip.com>
2026-04-27 05:40:23 +0600

    feat: add how-it-works and benefits section to homepage

--- app/page.tsx
+++ app/page.tsx
@@ -72,7 +72,7 @@ export default function HomePage() {
     <>
       <Hero toolCount={toolCount} />
       <HowItWorksStrip toolCount={toolCount} categoryCount={categoryCount} />
-      <CategoryQuickAccess />
+      <CategoryQuickAccess categories={categories as any} />
       <WhyToolblip />
       <FeaturedStrip />
       <CategoryGrid />
